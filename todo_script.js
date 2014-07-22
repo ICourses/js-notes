3402b0c79bc24ddbd59615732a9416354f998122
Katrine <may15@tut.by>
2014-07-22 23:50:29 +0200

    Function visualize update, separate function vis for display notes

--- todo_script.js
+++ todo_script.js
@@ -1,58 +1,53 @@
-window.todo = (function(){
+window.todo = (function () {
     return{
-        initial: function(settings){
+        initial: function (settings) {
 
-            function cleanList(){
+            function cleanList() {
                 document.getElementById(settings.listId).innerHTML = "";
             }
 
-            function cleanInput(){
+            function cleanInput() {
                 document.getElementById(settings.inputId).value = "";
             }
 
-            function addInArray( val ){
-                array.push( val );
+            function addInArray(val) {
+                array.push(val);
             }
 
-            function deleteFromArray( text){
-                var i = array.indexOf( text );
+            function deleteFromArray(text) {
+                var i = array.indexOf(text);
                 array.splice(i, 1);
             }
 
-            function visualize( str ){
+            function visualize(str) {
                 var Teg = document.createElement('div');
-                Teg.innerHTML = vis();
-                Teg.firstChild.textContent = str;
+                Teg.innerHTML = settings.vis(str);
                 document.getElementById(settings.listId).appendChild(Teg);
             }
 
-            function vis(){
-                return "<div id='list_div'><a class=" + settings.aTegClassName + "><img class=" + settings.imgClassName +" src=" +settings.image + "></a></div>";
-            }
-
-            function display ( array ){
-                for ( var i=0; i < array.length; i++ ){
+            function display(array) {
+                for (var i = 0; i < array.length; i++) {
                     visualize(array[i]);
                 }
             }
 
             var array = [];
 
-            document.getElementById(settings.buttonId).addEventListener("click", function() {
+            document.getElementById(settings.buttonId).addEventListener("click", function () {
                 if (document.getElementById(settings.inputId).value) {
-                    addInArray( document.getElementById(settings.inputId).value );
+                    addInArray(document.getElementById(settings.inputId).value);
                     cleanInput();
                     cleanList();
                     display(array);
                 }
-                else{
+                else {
                     alert("Enter a note for the entry in the list.");
                 }
             });
 
-            document.getElementById(settings.listId).addEventListener("click", function(event) {
-                if(event.target.className == "image"){
-                    deleteFromArray( event.target.parentElement.parentElement.firstChild.textContent );
+            document.getElementById(settings.listId).addEventListener("click", function (event) {
+                if (event.target.className == settings.imageClassName) {
+                    deleteFromArray(event.target.parentElement.parentElement.firstChild.textContent);
                     cleanList();
                     display(array);
                 }
